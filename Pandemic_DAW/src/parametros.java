c8815cf70733264170cf5cb9066aee47988ab621
DryaK12 <dvdmrlnc@gmail.com>
2023-04-19 17:44:11 +0200

    Parametros XML
    Arreglado despues de la correccion

--- Pandemic_DAW/src/parametros.java
+++ Pandemic_DAW/src/parametros.java
@@ -22,126 +22,195 @@ import org.xml.sax.SAXException;
 public class parametros {
 
 	public static void main(String[] args) {
-	    // Inicializar Scanner para leer la entrada del usuario
-	    Scanner scanner = new Scanner(System.in);
+		// Inicializar Scanner para leer la entrada del usuario
+		Scanner scanner = new Scanner(System.in);
 
-	    // Nombre del archivo XML con los parámetros del juego
-	    String fileName = "C:\\Users\\Mohamed\\Documents\\GitHub\\Pandemic_DAW\\Pandemic_DAW\\src\\parametros.xml";
-	    
-	    // Crear objeto DocumentBuilderFactory para obtener un objeto DocumentBuilder
-	    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		// Nombre del archivo XML con los parámetros del juego
+		String fileName = "C:\\Users\\Alumnat\\Desktop\\parametros\\parametros.xml";
 
-	    try {
-	        // Crear objeto DocumentBuilder a partir del objeto DocumentBuilderFactory
-	        DocumentBuilder builder = factory.newDocumentBuilder();
-	        // Leer el archivo XML y parsearlo en un objeto Document
-	        Document document = builder.parse(new File(fileName));
-	        // Normalizar el Document para asegurar la coherencia de los datos
-	        document.getDocumentElement().normalize();
+		// Crear objeto DocumentBuilderFactory para obtener un objeto DocumentBuilder
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 
-	        // Variable booleana para controlar el flujo del programa
-	        boolean salir = false;
-	        while (!salir) {
-	            // Mostrar el menú de opciones
-	            System.out.println("\nMENU");
-	            System.out.println("1. Mostrar parametros");
-	            System.out.println("2. Modificar parametros");
-	            System.out.println("3. Salir");
-	            System.out.print("Elija una opcion: ");
+		try {
+			// Crear objeto DocumentBuilder a partir del objeto DocumentBuilderFactory
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			// Leer el archivo XML y parsearlo en un objeto Document
+			Document document = builder.parse(new File(fileName));
+			// Normalizar el Document para asegurar la coherencia de los datos
+			document.getDocumentElement().normalize();
 
-	            // Leer la opción seleccionada por el usuario
-	            int opcion = scanner.nextInt();
+			// Variable booleana para controlar el flujo del programa
+			boolean salir = false;
+			while (!salir) {
+				// Mostrar el menú de opciones
+				System.out.println("\nMENU");
+				System.out.println("1. Mostrar parametros");
+				System.out.println("2. Modificar parametros");
+				System.out.println("3. Salir");
+				System.out.print("Elija una opcion: ");
 
-	            switch (opcion) {
-	                // Mostrar los parámetros actuales del juego
-	                case 1:
-	                    mostrarParametros(document);
-	                    break;
-	                // Modificar los parámetros del juego
-	                case 2:
-	                    modificarParametros(document, fileName);
-	                    break
-	                // Salir del programa
-	                case 3:
-	                    salir = true;
-	                    break
-	                // Opción no válida
-	                default:
-	                    System.out.println("Opcion no valida. Intente de nuevo.");
-	            }
-	        }
+				// Leer la opción seleccionada por el usuario
+				int opcion = scanner.nextInt();
 
-	    } catch (ParserConfigurationException | SAXException | IOException e) {
-	        // Manejar las excepciones que puedan surgir durante la ejecución del programa
-	        e.printStackTrace();
-	    }
+				switch (opcion) {
+				// Mostrar los parámetros actuales del juego
+				case 1:
+					mostrarParametros(document);
+					break;
+				// Modificar los parámetros del juego
+				case 2:
+					modificarParametros(document, fileName);
+					break;
+				// Salir del programa
+				case 3:
+					salir = true;
+					break;
+				// Opción no válida
+				default:
+					System.out.println("Opcion no valida. Intente de nuevo.");
+					break;
+				}
+			}
+
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			// Manejar las excepciones que puedan surgir durante la ejecución del programa
+			e.printStackTrace();
+		}
 	}
 
 	// Mostrar los parámetros actuales del juego
 	private static void mostrarParametros(Document document) {
-	    // Obtener una lista de nodos con el tag "parametros"
-	    NodeList nodeList = document.getElementsByTagName("parametros");
+		// Obtener una lista de nodos con el tag "parametros"
+		NodeList nodeList = document.getElementsByTagName("parametros");
 
-	    if (nodeList.getLength() > 0) {
-	        // Obtener el primer elemento "parametros"
-	        Element parametros = (Element) nodeList.item(0);
-            System.out.println("\nPARAMETROS\n");
-            System.out.println("Numero de ciudades infectadas al inicio: " + parametros.getElementsByTagName("numCiudadesInfectadasInicio").item(0).getTextContent());
-            System.out.println("Numero de ciudades infectadas por ronda: " + parametros.getElementsByTagName("numCuidadesInfectadasRonda").item(0).getTextContent());
-            System.out.println("Numero de enfermedades activas para perder el juego: " + parametros.getElementsByTagName("numEnfermedadesActivasDerrota").item(0).getTextContent());
-            System.out.println("Numero de brotes para perder el juego: " + parametros.getElementsByTagName("numBrotesDerrota").item(0).getTextContent());
-        } else {
-            System.out.println("No se encontraron parametros en el archivo.");
-        }
-    }
+		if (nodeList.getLength() > 0) {
+			// Obtener el primer elemento "parametros"
+			Element parametros = (Element) nodeList.item(0);
+			System.out.println("\nPARAMETROS\n");
+			System.out.println("Numero de ciudades infectadas al inicio: "
+					+ parametros.getElementsByTagName("numCiudadesInfectadasInicio").item(0).getTextContent());
+			System.out.println("Numero de ciudades infectadas por ronda: "
+					+ parametros.getElementsByTagName("numCuidadesInfectadasRonda").item(0).getTextContent());
+			System.out.println("Numero de enfermedades activas para perder el juego: "
+					+ parametros.getElementsByTagName("numEnfermedadesActivasDerrota").item(0).getTextContent());
+			System.out.println("Numero de brotes para perder el juego: "
+					+ parametros.getElementsByTagName("numBrotesDerrota").item(0).getTextContent());
+			System.out.println("-------------------------------------------------------------------------");
+		} else {
+			System.out.println("No se encontraron parametros en el archivo.");
+		}
+	}
+
 //modificar los parametros
-    private static void modificarParametros(Document document, String fileName) {
-        NodeList nodeList = document.getElementsByTagName("parametros");
+	private static void modificarParametros(Document document, String fileName) {
+		NodeList nodeList = document.getElementsByTagName("parametros");
+		int opcion = 0;
+		boolean repetir = true;
 		Scanner scanner = new Scanner(System.in);
-		
-        if (nodeList.getLength() > 0) {
-            Element parametros = (Element) nodeList.item(0);
-            System.out.println("\nMODIFICAR PARAMETROS\n");
-            System.out.print("Numero de ciudades infectadas al inicio (actual: " + parametros.getElementsByTagName("numCiudadesInfectadasInicio").item(0).getTextContent() + "): ");
-            String numCiudadesInfectadasInicio = scanner.nextLine();
-            parametros.getElementsByTagName("numCiudadesInfectadasInicio").item(0).setTextContent(numCiudadesInfectadasInicio);
+		Scanner scanner2 = new Scanner(System.in);
 
-            System.out.print("Numero de ciudades infectadas por ronda (actual: " + parametros.getElementsByTagName("numCuidadesInfectadasRonda").item(0).getTextContent() + "): ");
-            String numCuidadesInfectadasRonda = scanner.nextLine();
-            parametros.getElementsByTagName("numCuidadesInfectadasRonda").item(0).setTextContent(numCuidadesInfectadasRonda);
+		repetir = true;
 
-            System.out.print("Numero de enfermedades activas para perder el juego (actual: " + parametros.getElementsByTagName("numEnfermedadesActivasDerrota").item(0).getTextContent() + "): ");
-            String numEnfermedadesActivasDerrota = scanner.nextLine();
-            parametros.getElementsByTagName("numEnfermedadesActivasDerrota").item(0).setTextContent(numEnfermedadesActivasDerrota);
+		while (repetir == true) {
+			System.out.println("-------------------------------------------------------------------------");
+			System.out.println("Elige el parametro que quieras modificar");
+			System.out.println("1: Numero de ciudades infectadas al inicio");
+			System.out.println("2: Numero de ciudades infectadas por ronda");
+			System.out.println("3: Numero de enfermedades activas para perder el juego");
+			System.out.println("4: Numero de brotes para perder el juego");
+			System.out.println("5: Dejar de modificar parametros");
+			opcion = scanner2.nextInt();
+			System.out.println("-------------------------------------------------------------------------");
+			if (opcion == 1) {
+				Element parametros = (Element) nodeList.item(0);
+				System.out.print("Numero de ciudades infectadas al inicio (actual: "
+						+ parametros.getElementsByTagName("numCiudadesInfectadasInicio").item(0).getTextContent()
+						+ "): ");
+				String numCiudadesInfectadasInicio = scanner.nextLine();
+				if (numCiudadesInfectadasInicio.equals("")) {
 
-            System.out.print("Numero de brotes para perder el juego (actual: " + parametros.getElementsByTagName("numBrotesDerrota").item(0).getTextContent() + "): ");
-            String numBrotesDerrota = scanner.nextLine();
-            parametros.getElementsByTagName("numBrotesDerrota").item(0).setTextContent(numBrotesDerrota);
+				} else {
+					parametros.getElementsByTagName("numCiudadesInfectadasInicio").item(0)
+							.setTextContent(numCiudadesInfectadasInicio);
+					guardarCambios(document, fileName);
+					System.out.println("\nCambios guardados con exito.");
+				}
+			}
+			////////////////////////////////////////////////////////////////////////////////////////
+			else if (opcion == 2) {
+				Element parametros = (Element) nodeList.item(0);
+				System.out.print("Numero de ciudades infectadas por ronda (actual: "
+						+ parametros.getElementsByTagName("numCuidadesInfectadasRonda").item(0).getTextContent()
+						+ "): ");
+				String numCuidadesInfectadasRonda = scanner.nextLine();
+				if (numCuidadesInfectadasRonda.equals("")) {
 
-            guardarCambios(document, fileName);
-            System.out.println("\nCambios guardados con exito.");
+				} else {
+					parametros.getElementsByTagName("numCuidadesInfectadasRonda").item(0)
+							.setTextContent(numCuidadesInfectadasRonda);
+					guardarCambios(document, fileName);
+					System.out.println("\nCambios guardados con exito.");
+				}
+			}
+			////////////////////////////////////////////////////////////////////////////////////////
+			else if (opcion == 3) {
+				Element parametros = (Element) nodeList.item(0);
+				System.out.print("Numero de enfermedades activas para perder el juego (actual: "
+						+ parametros.getElementsByTagName("numEnfermedadesActivasDerrota").item(0).getTextContent()
+						+ "): ");
+				String numEnfermedadesActivasDerrota = scanner.nextLine();
+				if (numEnfermedadesActivasDerrota.equals("")) {
 
-        } else {
-            System.out.println("No se encontraron parametros en el archivo.");
-        }
-    }
+				} else {
+					parametros.getElementsByTagName("numEnfermedadesActivasDerrota").item(0)
+							.setTextContent(numEnfermedadesActivasDerrota);
+
+					guardarCambios(document, fileName);
+					System.out.println("\nCambios guardados con exito.");
+				}
+			}
+			////////////////////////////////////////////////////////////////////////////////////////
+			else if (opcion == 4) {
+				Element parametros = (Element) nodeList.item(0);
+				System.out.print("Numero de brotes para perder el juego (actual: "
+						+ parametros.getElementsByTagName("numBrotesDerrota").item(0).getTextContent() + "): ");
+				String numBrotesDerrota = scanner.nextLine();
+
+				if (numBrotesDerrota.equals("")) {
+
+				} else {
+
+					parametros.getElementsByTagName("numBrotesDerrota").item(0).setTextContent(numBrotesDerrota);
+					guardarCambios(document, fileName);
+					System.out.println("\nCambios guardados con exito.");
+				}
+
+			}
+
+			else if (opcion == 5) {
+				repetir = false;
+			}
+
+		}
+
+	}
+
 // guardar los cambios
-    private static void guardarCambios(Document document, String fileName) {
-        try {
-            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-            Transformer transformer = transformerFactory.newTransformer();
-            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+	private static void guardarCambios(Document document, String fileName) {
+		try {
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
 
-            DOMSource source = new DOMSource(document);
-            StreamResult result = new StreamResult(new FileWriter(fileName));
+			DOMSource source = new DOMSource(document);
+			StreamResult result = new StreamResult(new FileWriter(fileName));
 
-            transformer.transform(source, result);
-        } catch (IOException | TransformerException e) {
-            e.printStackTrace();
-        }
-    }
+			transformer.transform(source, result);
+		} catch (IOException | TransformerException e) {
+			e.printStackTrace();
+		}
+	}
 }
-
 
 // Mohamed Oundouh - David Mir 
